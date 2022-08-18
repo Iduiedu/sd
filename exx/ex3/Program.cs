678156775e79328fd4d88e03518ebf1131880a3e
Anna <troshkova.anna@mail.ru>
2022-08-18 08:01:52 +0400

    создание проекта

--- exx/ex3/Program.cs
+++ exx/ex3/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 3;
+int numberB = 5;
+int results = numberA + numberB;
+Console.WriteLine(results);
