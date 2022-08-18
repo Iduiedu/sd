524c76e5ff61eeec092905c1a4265100178ff188
Anna <troshkova.anna@mail.ru>
2022-08-18 10:14:59 +0400

    8

---
+++ exx/ex5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() =="аня")
+{
+    Console.WriteLine("Ура, это же АНЯ!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
